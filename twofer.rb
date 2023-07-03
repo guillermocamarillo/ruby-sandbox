6d4e6909383fc93fbf1dc8b3f7cc29e340f99890
guillermocamarillo <137141464+guillermocamarillo@users.noreply.github.com>
2023-07-03 17:18:33 +0000

    twofer program

---
+++ twofer.rb
@@ -0,0 +1,10 @@
+name = ["raghu", "Bob", ""].sample
+
+length = name.length
+
+if(length == 0)
+pp "One for you, one for me."
+else
+  name = name.capitalize
+  pp "One for " + name + ", one for me."
+end
